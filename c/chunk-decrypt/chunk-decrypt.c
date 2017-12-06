1f7fd6732df626123100207c698a9e78e102c882
Morgan Philie <morgan@MacBook-Pro.local>
2017-12-06 13:07:41 -0500

    chunk decrypt functioning

--- c/chunk-decrypt/chunk-decrypt.c
+++ c/chunk-decrypt/chunk-decrypt.c
@@ -11,33 +11,34 @@
 #include <errno.h>
 
 int main() {
+    
 	// TODO: Update the below string to your encrypted value from IonicHelloWorld, e.g. "~!2!ABcd...AA!Nk...Wp!"
-    char* encryptedText = "{SET_ENCRYPTED_STRING_HERE}";
+    char *encryptedText = "~!2!HVzG334Nm24!6S8oWtCJSoRnt544OZRUOuRu2wrsniiNFIt3dw!";
 	if (strcmp(encryptedText, "{SET_ENCRYPTED_STRING_HERE}") == 0) {
-		perror("You MUST set the output from IonicHelloWorld (or similar) to decrypt.\n");
+		printf("You MUST set the output from IonicHelloWorld (or similar) to decrypt.\n");
 		exit(-1);
 	}
 
 
     // Setup an agent object to talk to Ionic
-    ionic_profile_persistor_t *profileLeaderOpt = ionic_profile_persistor_create_default();
-    ionic_agent_t *agent = ionic_agent_create(profileLeaderOpt, NULL);
+    ionic_profile_persistor_t *profileLoaderOpt = ionic_profile_persistor_create_default();
+    ionic_agent_t *agent = ionic_agent_create(profileLoaderOpt, NULL);
 
     // Setup a Chunk Crypto object to handle Ionic encryption
     ionic_chunkcipher_t *chunkCrypto = ionic_chunkcipher_create_auto(agent);
 
     // Decrypt the string using an Ionic-managed Key
-    ionic_bytes_t *plaintext;
-    int nErrorCode = ionic_chunkcipher_decrypt_bytes(chunkCrypto, encryptedText, &plaintext);
+    char *plaintext;
+    int nErrorCode = ionic_chunkcipher_decrypt_str(chunkCrypto, encryptedText, &plaintext);
 
     // Validate the response
-    if (nErrorCode != ISCRYPTO_OK) {
-        perror("Error decrypting: ");//%s", getErrorCodeString(nErrorCode));
+    if (nErrorCode != ISC_OK) {
+        printf("Error decrypting: %s\n", ionic_get_error_str(nErrorCode));
 		exit(-2);
     }
 
     printf("Chunk-Encrypted String: %s\n", encryptedText);
-    printf("Decrypted String: ");//%s\n", plaintext);
+    printf("Decrypted String: %s\n", plaintext);
     
     return 0;
 }
