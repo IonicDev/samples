b5839bef79af56f54fb9c27a2398318264323185
Morgan Philie <morgan@MacBook-Pro.local>
2017-12-06 09:57:10 -0500

    file encrypt and decrypt functioning

--- c/file-crypto-auto-decrypt/file-crypto-auto-decrypt.c
+++ c/file-crypto-auto-decrypt/file-crypto-auto-decrypt.c
@@ -4,34 +4,33 @@
  * and the Privacy Policy (https://www.ionic.com/privacy-notice/).
  */
 
-#include <iostream>
-#include <ISAgent.h>
-#include <ISFileCrypto.h>
-#include <ISAgentSDKError.h>
+#include <ISAgentSDKC.h>
+#include <ISAgentSDKCFileCrypto.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
 
 int main()
 {
-	std::string inFilePath("example_file_encrypted.docx");
-	std::string outFilePath("example_file_decrypted.docx");
+	char inFilePath[] = "example_file_encrypted.txt";
+	char outFilePath[] = "example_file_decrypted.txt";
 
 	// Setup an agent object to talk to Ionic
-    ionic_profile_persistor_t *profileLeaderOpt = ionic_profile_persistor_create_default();
-    ionic_agent_t *agent = ionic_agent_create(profileLeaderOpt, NULL);
+    ionic_profile_persistor_t *profileLoaderOpt = ionic_profile_persistor_create_default();
+    ionic_agent_t *agent = ionic_agent_create(profileLoaderOpt, NULL);
 
 	//Encrypt the file using the classification
-	ISFileCryptoCipherAuto cipher(agent);
-	int nErrorCode = cipher.decrypt(inFilePath, outFilePath);
+	ionic_filecipher_t *fileCipher = ionic_filecipher_create_auto(agent);
+	int nErrorCode = ionic_filecipher_decrypt(fileCipher, inFilePath, outFilePath);
 
 	// Validate the response
-	if (nErrorCode != ISCRYPTO_OK) {
-		std::cerr << "Error decrypting " << inFilePath << ": " << ISAgentSDKError::getErrorCodeString(nErrorCode) << std::endl;
-		std::cout << "Press return to exit.";
-		std::getchar();
-		return -2;
+	if (nErrorCode != ISC_OK) {
+		printf("Error decrypting: %s\n", ionic_get_error_str(nErrorCode));
+		exit(-2);
 	}
 
-	std::cout << "Decrypted file " << inFilePath << " to " << outFilePath << "." << std::endl;
-	std::cout << "Press return to exit.";
-	std::getchar();
+	printf("Decrypted file %s to %s\n", inFilePath, outFilePath);
+
 	return 0;
 }
---
+++ c/file-crypto-auto-encrypt/file-crypto-auto-encrypt.c
@@ -0,0 +1,48 @@
+/*
+ * (c) 2017 Ionic Security Inc.
+ * By using this code, I agree to the Terms & Conditions (https://www.ionic.com/terms-of-use/)
+ * and the Privacy Policy (https://www.ionic.com/privacy-notice/).
+ */
+
+#include <ISAgentSDKC.h>
+#include <ISAgentSDKCFileCrypto.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+int main()
+{
+	char inFilePath[] = "example_file.txt";
+	char outFilePath[] = "example_file_encrypted.txt";
+    
+    // Setup an agent object to talk to Ionic
+    ionic_profile_persistor_t *profileLoaderOpt = ionic_profile_persistor_create_default();
+    ionic_agent_t *agent = ionic_agent_create(profileLoaderOpt, NULL);
+    
+    /*
+     * NOTE: Ensure you have an active profile set. You can do this by downloading the
+     * Ionic Manager application and following the steps for enrolling your device.
+     */
+
+    // Create and apply classification
+    ionic_attributesmap_t *attrsMap = ionic_attributesmap_create();
+    int nAttrError = ionic_attributesmap_set(attrsMap, "classification", "Restricted");
+    if (nAttrError != ISC_OK) {
+        printf("Error setting attribute map: %s\n", ionic_get_error_str(nAttrError));
+		exit(-1);
+    }
+    
+    // Encrypt the file using the classification
+	ionic_filecipher_t *fileCipher = ionic_filecipher_create_auto(agent);
+	int nErrorCode = ionic_filecipher_encrypt2(fileCipher, inFilePath, outFilePath, attrsMap, NULL, NULL, NULL, NULL);
+	
+	// Validate the response
+	if (nErrorCode != ISC_OK) {
+		printf("Error encrypting: %s\n", ionic_get_error_str(nErrorCode));
+		exit(-2);
+	}
+
+	printf("Encrypted file %s to %s\n", inFilePath, outFilePath);
+	return 0;
+}
