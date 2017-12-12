56d301cbe764e0b30b5814e52cca7783ff644717
Morgan Philie <morgan@MacBook-Pro.local>
2017-12-12 09:44:56 -0500

    excess indentations

--- c/file-crypto-auto-decrypt/file-crypto-auto-decrypt.c
+++ c/file-crypto-auto-decrypt/file-crypto-auto-decrypt.c
@@ -16,22 +16,22 @@ int main()
 {
     // Setup an agent object to talk to Ionic
 #if __linux__
-        //NOTE: On Linux, you must add additional code here, see "Getting Started" for C++ on Linux.
-        char *plainSepPath;
-        if (!makeLinuxHomeDirPath(".ionicsecurity/profiles.pt", plainSepPath)) { // Makes the absolute path for ~/.ionic/profiles.pt
-            printf("Error getting home directory path.\n");
-            exit(-1);
-        }
-        ionic_profile_persistor_t *plainPersistor = ionic_profile_persistor_create_plaintext_file(plainSepPath);
-        printf("Initializing agent...\n");
-        ionic_agent_t *agent = ionic_agent_create(plainPersistor, NULL);
+    //NOTE: On Linux, you must add additional code here, see "Getting Started" for C++ on Linux.
+    char *plainSepPath;
+    if (!makeLinuxHomeDirPath(".ionicsecurity/profiles.pt", plainSepPath)) { // Makes the absolute path for ~/.ionic/profiles.pt
+        printf("Error getting home directory path.\n");
+        exit(-1);
+    }
+    ionic_profile_persistor_t *plainPersistor = ionic_profile_persistor_create_plaintext_file(plainSepPath);
+    printf("Initializing agent...\n");
+    ionic_agent_t *agent = ionic_agent_create(plainPersistor, NULL);
 
-        printf("A plaintext SEP was loaded from %s\n", plainSepPath);
+    printf("A plaintext SEP was loaded from %s\n", plainSepPath);
 #else
-        // Setup an agent object to talk to Ionic
-        printf("Initializing agent...\n");
-        ionic_profile_persistor_t *profileLoaderOpt = ionic_profile_persistor_create_default();
-        ionic_agent_t *agent = ionic_agent_create(profileLoaderOpt, NULL);
+    // Setup an agent object to talk to Ionic
+    printf("Initializing agent...\n");
+    ionic_profile_persistor_t *profileLoaderOpt = ionic_profile_persistor_create_default();
+    ionic_agent_t *agent = ionic_agent_create(profileLoaderOpt, NULL);
 
 #endif
 
