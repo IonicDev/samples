47f31dce50fc5f91a55dff65d0dbde81bc0b3720
Ionic <ryan@ionicsecurity.com>
2018-02-25 14:41:49 -0500

    Update terms of use link.

--- java-jni/file-crypto-auto-encrypt/src/main/java/com/ionicsecurity/examples/FileCryptoCoverPageImpl.java
+++ java-jni/file-crypto-auto-encrypt/src/main/java/com/ionicsecurity/examples/FileCryptoCoverPageImpl.java
@@ -1,7 +1,7 @@
 /*
- * (c) 2017 Ionic Security Inc. By using this code, I agree to the Terms & Conditions
- * (https://www.ionic.com/terms-of-use/) and the Privacy Policy
- * (https://www.ionic.com/privacy-notice/).
+ * (c) 2017 Ionic Security Inc.
+ * By using this code, I agree to the Terms & Conditions (https://dev.ionic.com/use.html)
+ * and the Privacy Policy (https://www.ionic.com/privacy-notice/).
  */
 
 package com.ionicsecurity.examples;
--- java-jni/ionic-helloworld/src/main/java/com/ionic/ihw/IonicHelloWorld.java
+++ java-jni/ionic-helloworld/src/main/java/com/ionic/ihw/IonicHelloWorld.java
@@ -1,6 +1,6 @@
 /*
  * (c) 2017 Ionic Security Inc.
- * By using this code, I agree to the Terms & Conditions (https://www.ionic.com/terms-of-use/)
+ * By using this code, I agree to the Terms & Conditions (https://dev.ionic.com/use.html)
  * and the Privacy Policy (https://www.ionic.com/privacy-notice/).
  */
 
