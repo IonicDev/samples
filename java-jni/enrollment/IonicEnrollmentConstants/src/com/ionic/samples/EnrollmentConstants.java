53b6085a8bc88a8f394ba989b959dde428bddbc9
Ionic <ryan@ionicsecurity.com>
2018-02-25 14:42:55 -0500

    Added gitignores, added copyright on one file.

--- java-jni/enrollment/IonicEnrollmentConstants/src/com/ionic/samples/EnrollmentConstants.java
+++ java-jni/enrollment/IonicEnrollmentConstants/src/com/ionic/samples/EnrollmentConstants.java
@@ -1,3 +1,9 @@
+/*
+ * (c) 2017 Ionic Security Inc.
+ * By using this code, I agree to the Terms & Conditions (https://dev.ionic.com/use.html)
+ * and the Privacy Policy (https://www.ionic.com/privacy-notice/).
+ */
+
 package com.ionic.examples;
 
 public class EnrollmentConstants {
