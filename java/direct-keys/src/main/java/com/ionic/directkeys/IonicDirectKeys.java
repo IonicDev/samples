77dc0926215e9aef28e2d4b617e577117cca4be7
Paul Grey <pgrey@ionicsecurity.com>
2018-03-20 16:58:30 -0400

    fix directkeys (package name convention)

--- java/direct-keys/src/main/java/com/ionic/directkeys/IonicDirectKeys.java
+++ java/direct-keys/src/main/java/com/ionic/directkeys/IonicDirectKeys.java
@@ -4,7 +4,7 @@
  * and the Privacy Policy (https://www.ionic.com/privacy-notice/).
  */
 
-package ionic.directkeys;
+package com.ionic.directkeys;
 import com.ionic.sdk.error.SdkException;
 import com.ionic.sdk.device.profile.persistor.DeviceProfilePersistorPlainText;
 import com.ionic.sdk.agent.Agent;
@@ -24,8 +24,8 @@ public class IonicDirectKeys
         // Setup an agent object to talk to Ionic
         Agent agent = new Agent();
         try {
-            String profilePath = System.getproperty("user.home") + "/.ionicsecurity/profiles.pt";
-            DeviceProfilePersistorPlainText ptPersistor = new DeviceProfilePersistorPlainText(sProfilePath);
+            String profilePath = System.getProperty("user.home") + "/.ionicsecurity/profiles.pt";
+            DeviceProfilePersistorPlainText ptPersistor = new DeviceProfilePersistorPlainText(profilePath);
             agent.initialize(ptPersistor);
         } catch (SdkException e) {
             System.out.println("Failed to initialize agent:");
