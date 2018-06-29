8269953a8b677486521aaab3e6e508c3071249f7
Taylor Centers <taylor@is-mbp-taylor2.in.ionicsecurity.com>
2018-06-29 19:01:48 -0400

    ionic helloworld for javascript

---
+++ javascript/ionic-helloworld/helloWorld.js
@@ -0,0 +1,55 @@
+/*
+ * (c) 2018 Ionic Security Inc.
+ * By using this code, I agree to the Terms & Conditions (https://dev.ionic.com/use.html)
+ * and the Privacy Policy (https://www.ionic.com/privacy-notice/).
+ */
+
+const appData = {
+    appId: 'ionic-js-samples',
+    userId: 'developer',
+    userAuth: 'password123',
+    enrollmentUrl: 'https://dev-enrollment.ionic.com/keyspace/HVzG/register'
+}
+
+const main = async () => {
+    
+    const message = "secret message"
+
+    // initialize agent
+    const agent = new window.IonicSdk.ISAgent('https://dev-api.ionic.com/jssdk/latest/');
+
+    await agent.loadUser(appData).catch(async (error) => {
+        if (
+            error &&
+            error.sdkResponseCode &&
+            (error.sdkResponseCode === 40022 || error.sdkResponseCode === 40002)
+        ) {
+            resp = await agent.enrollUser(appData)
+
+            if(resp) {
+                if (resp.redirect) {
+                    window.open(resp.redirect);
+                    return resp.Notifier;
+                }
+            }
+            else {
+                console.log(`Error loading profile: ${error}`)
+                return Promise.reject("Error enrolling");
+            }
+        }
+        else {
+            console.log(`Error loading profile: ${error}`)
+            return
+        }
+    })
+
+    // encrypt message
+    const encryptResponse = await agent.encryptStringChunkCipher({stringData: message})
+    const ciphertext = encryptResponse.stringChunk
+
+    // display data
+    console.log(`Plain Text  : ${message}`)
+    console.log(`Ionic Chunk Encrypted Text: ${ciphertext}`)
+}
+
+main();
