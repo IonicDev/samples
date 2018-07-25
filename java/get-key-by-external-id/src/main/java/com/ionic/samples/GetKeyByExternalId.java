c5eb66cd217fcfabb6c38308954613cbe21b7eec
Rick Ehrhart <rick@ionicsecurity.com>
2018-07-25 12:42:29 -0700

    Added Java get-key-by-external-id

--- java/get-key-by-external-id/src/main/java/com/ionic/samples/GetKeyByExternalId.java
+++ java/get-key-by-external-id/src/main/java/com/ionic/samples/GetKeyByExternalId.java
@@ -3,21 +3,71 @@
  * By using this code, I agree to the Terms & Conditions (https://dev.ionic.com/use.html)
  * and the Privacy Policy (https://www.ionic.com/privacy-notice/).
  * 
- * Developed with Ionic Java SDK 2.1.0
+ * Developed with Ionic Java SDK 2.2.1
  */
 
 package com.ionic.samples;
 
 import com.ionic.sdk.device.profile.persistor.DeviceProfilePersistorPassword;
 import com.ionic.sdk.agent.Agent;
+import com.ionic.sdk.agent.request.getkey.GetKeysRequest;
 import com.ionic.sdk.agent.request.getkey.GetKeysResponse;
 import com.ionic.sdk.error.IonicException;
+import java.util.List;
+import javax.json.JsonValue;
 import javax.xml.bind.DatatypeConverter;
 
 public class GetKeyByExternalId
 {
     public static void main(String[] args)
     {
-        // NOT SUPPORTED YET
+        String externalId = "02e09520-e52c-42aa-b21c-a60698cf31a2";
+
+        // read persistor password from environment variable
+        String persistorPassword = System.getenv("IONIC_PERSISTOR_PASSWORD");
+        if (persistorPassword == null) {
+            System.out.println("[!] Please provide the persistor password as env variable: IONIC_PERSISTOR_PASSWORD");
+            System.exit(1);
+        }        
+
+        // Initialize agent.
+        Agent agent = new Agent();
+        try {
+            String persistorPath = System.getProperty("user.home") + "/.ionicsecurity/profiles.pw";
+            DeviceProfilePersistorPassword persistor = new DeviceProfilePersistorPassword(persistorPath);
+            persistor.setPassword(persistorPassword);
+            agent.initialize(persistor);
+        } catch(IonicException e) {
+            System.out.println(e.getMessage());
+            System.exit(1);
+        }
+
+        // Build a KeyRequest with an external ID.
+        GetKeysRequest keysRequest = new GetKeysRequest();
+        keysRequest.addExternalId(externalId);
+
+        // Get one or more keys with an external ID.
+        GetKeysResponse keysResponse = null;
+        List<GetKeysResponse.Key> keys = null;
+        try {
+            keys = agent.getKeys(keysRequest).getKeys();
+        } catch(IonicException e) {
+            System.out.println(e.getMessage());
+            System.exit(1);
+        }
+
+        if (keys.size() == 0) {
+            System.out.println("No keys returned for external IDs.");
+            System.exit(1);
+        }
+            
+        // Display fetched keys.
+        for (GetKeysResponse.Key key : keys) {
+            System.out.println("---");
+            System.out.println("KeyId        : " + key.getId());
+            System.out.println("KeyBytes     : " + DatatypeConverter.printHexBinary(key.getKey()));
+            System.out.println("FixedAttrs   : " + key.getAttributesMap());
+            System.out.println("MutableAttrs : " + key.getMutableAttributes());
+        }
     }
 }
