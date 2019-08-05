ec5183474cbcbc5dff8ee275da1c625efbcc719c
Kevin Karpinski <kevin@ionicsecurity.com>
2019-08-05 16:42:20 -0400

    fix for crypto-file-cipher-openxml/main.py

--- python/crypto-file-cipher-openxml/main.py
+++ python/crypto-file-cipher-openxml/main.py
@@ -9,6 +9,13 @@ import ionicsdk
 file_original = '../../sample-data/files/Message.docx'
 file_ciphertext = './Message-Protected.docx'
 file_plaintext = './Message.docx'
+source_dir = 'github-samples/python/crypto-file-cipher-openxml'
+this_dir = os.getcwd()
+
+# run only from source directory
+if not this_dir.endswith(source_dir): 
+    print("[!] Please run this sample from inside " + source_dir)
+    sys.exit(1)
 
 # read persistor password from environment variable
 persistorPassword = os.environ.get('IONIC_PERSISTOR_PASSWORD')
