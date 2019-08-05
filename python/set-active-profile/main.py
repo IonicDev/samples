1b0a320d84f097b5882d58f4aa1640f571a40b43
Kevin Karpinski <kevin@ionicsecurity.com>
2019-08-05 16:27:35 -0400

    fix for set-active-profile

--- python/set-active-profile/main.py
+++ python/set-active-profile/main.py
@@ -8,6 +8,13 @@ import ionicsdk
 
 profile_id = 'ABcd.1.48sdf0-cs80-5802-sd80-d8s0df80sdfj'
 persistor_path = os.path.join("..", "..", "sample-data", "persistors", "sample-persistor.pt")
+source_dir = 'github-samples/python/set-active-profile'
+this_dir = os.getcwd()
+
+# run only from source directory
+if not this_dir.endswith(source_dir):
+    print("[!] Please run this sample from inside " + source_dir)
+    sys.exit(1)
 
 # Initialize agent with plaintext persistor.
 try:
