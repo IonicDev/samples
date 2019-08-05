e9d1fd95dc12de10adc7ed341657aae8f01cc724
Kevin Karpinski <kevin@ionicsecurity.com>
2019-08-05 13:26:38 -0400

    check-profile fix

--- python/check-profiles/main.py
+++ python/check-profiles/main.py
@@ -2,12 +2,21 @@
 # By using this code, I agree to the Terms & Conditions (https://dev.ionic.com/use.html)
 # and the Privacy Policy (https://www.ionic.com/privacy-notice/).
 
+import os
 import sys
 import ionicsdk
 
-# initialize agent with default persistor
+# read persistor password from environment variable
+persistorPassword = os.environ.get('IONIC_PERSISTOR_PASSWORD')
+if (persistorPassword == None):
+    print("[!] Please provide the persistor password as env variable: IONIC_PERSISTOR_PASSWORD")
+    sys.exit(1)
+
+# initialize agent with password persistor
 try:
-    agent = ionicsdk.Agent()
+    persistorPath = os.path.expanduser("~/.ionicsecurity/profiles.pw")
+    persistor = ionicsdk.DeviceProfilePersistorPasswordFile(persistorPath, persistorPassword)
+    agent = ionicsdk.Agent(None, persistor)
 except ionicsdk.exceptions.IonicException as e:
     print("Error initializing agent: {0}".format(e.message))
     sys.exit(1)
@@ -28,6 +37,3 @@ if agent.hasanyprofiles():
         print ("There is not an active device profile selected on this device.")
 else:
     print ("There are no device profiles on this device.")
-
-raw_input("\nPress Enter to continue...")
-
