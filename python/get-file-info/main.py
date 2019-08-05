3bce5cd6f4bcabee9cc559b520a8af93cc72e84c
Kevin Karpinski <kevin@ionicsecurity.com>
2019-08-05 16:20:42 -0400

    fixes for get-file-info

--- python/get-file-info/main.py
+++ python/get-file-info/main.py
@@ -2,22 +2,33 @@
 # By using this code, I agree to the Terms & Conditions (https://dev.ionic.com/use.html)
 # and the Privacy Policy (https://www.ionic.com/privacy-notice/).
 
+import os
+import sys
 import ionicsdk
 
-# prompt user for path to file
-filepath = input("Please enter the path to the file: ")
+plaintext_file = '../../sample-data/files/Message.pdf'
+protected_file = '../../sample-data/files/protected-sample.txt'
+source_dir = 'github-samples/python/get-file-info'
+this_dir = os.getcwd()
+
+# run only from source directory
+if not this_dir.endswith(source_dir):
+    print ("[!] Please run this sample from inside" + source_dir)
+    sys.exit(1)
+
+print ("plaintext file: " + plaintext_file)
+print ("protected file: " + protected_file)
 
 # get file info
 try:
-    fileinfo = ionicsdk.FileCrypto.getinfo(filepath)
+    for filepath in [plaintext_file, protected_file]:
+        fileinfo = ionicsdk.FileCrypto.getinfo(filepath)
+
+        # display file info
+        if fileinfo.isencrypted:
+            print ("The file \"{}\" is encrypted using key {}.".format(filepath, fileinfo.keyid))
+        else:
+            print ("The file \"{}\" is not encrypted.".format(filepath))
 except ionicsdk.exceptions.IonicException as e:
     print ("Error reading file: {}".format(e.message))
     sys.exit(1)
-
-# display file info
-if fileinfo.isencrypted:
-    print ("The file \"{}\" is encrypted using key {}.".format(filepath, fileinfo.keyid))
-else:
-    print ("The file \"{}\" is not encrypted.".format(filepath))
-
-raw_input("\nPress Enter to continue...")
