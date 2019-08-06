5cf4bf2168a0b2fd9cf82d421f92b82208e147bb
Kevin Karpinski <kevin@ionicsecurity.com>
2019-08-06 13:14:10 -0400

    update and or add copyright statements

--- python/initialize-agent-with-password-persistor/main.py
+++ python/initialize-agent-with-password-persistor/main.py
@@ -1,3 +1,7 @@
+# (c) 2018-2019 Ionic Security Inc.
+# By using this code, I agree to the Terms & Conditions (https://dev.ionic.com/use.html)
+# and the Privacy Policy (https://www.ionic.com/privacy-notice/).
+
 import os
 import sys
 import ionicsdk
