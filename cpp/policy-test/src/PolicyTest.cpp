d69536269380a3131bc71b860947f3d74b3f9600
Rick Ehrhart <rick@ionicsecurity.com>
2019-08-20 13:34:44 -0700

    C++ policy-text and .gitignore change for CMakeLists.txt

---
+++ cpp/policy-test/src/PolicyTest.cpp
@@ -0,0 +1,88 @@
+/*
+ * (c) 2019 Ionic Security Inc.
+ * By using this code, I agree to the Terms & Conditions (https://dev.ionic.com/use.html)
+ * and the Privacy Policy (https://www.ionic.com/privacy-notice/).
+ */
+
+#include "ISAgent.h"
+#include "ISAgentSDKError.h"
+#include <ISChunkCrypto.h>
+#include <ISChunkCryptoEncryptAttributes.h>
+#include <stdio.h>
+#include <cstdlib>
+#include <iostream>
+
+#ifdef _WIN32
+    #define HOMEVAR "USERPROFILE"
+#else 
+    #define HOMEVAR "HOME"
+#endif
+
+int main(int argc, char* argv[]) {
+
+    int nErrorCode;
+    std::string appName = "policy-test";
+    std::string appVersion = "1.0.0";
+
+    std::string message = "secret message requiring secrect clearance";
+
+    // read persistor password from environment variable
+    char* cpersistorPassword = std::getenv("IONIC_PERSISTOR_PASSWORD");
+    if (cpersistorPassword == NULL) {
+        std::cerr << "[!] Please provide the persistor password as env variable: IONIC_PERSISTOR_PASSWORD" << std::endl;
+        exit(1);
+    }
+    std::string persistorPassword = std::string(cpersistorPassword);
+
+    // initialize agent with password persistor
+    std::string persistorPath = std::string(std::getenv(HOMEVAR)) + "/.ionicsecurity/profiles.pw";
+    ISAgentDeviceProfilePersistorPassword persistor;
+    persistor.setFilePath(persistorPath);
+    persistor.setPassword(persistorPassword);
+    ISAgent agent;
+    nErrorCode = agent.initialize(persistor);
+    if (nErrorCode != ISAGENT_OK) {
+        std::cerr << "Failed to initialize agent from password persistor (" << persistorPath << ")" << std::endl;
+        std::cerr << ISAgentSDKError::getErrorCodeString(nErrorCode) << std::endl;
+        exit(1);
+    }
+
+    // set app metadata
+    agent.setMetadata("ionic-application-name", appName);
+    agent.setMetadata("ionic-application-version", appVersion);
+
+    // define data markings
+    std::map< std::string, std::vector< std::string > > dataMarkings;
+    std::vector<std::string> clearanceLevel;
+    clearanceLevel.push_back("secret");
+    dataMarkings["clearance-level"] = clearanceLevel;
+    ISChunkCryptoEncryptAttributes cipherAttributes;
+    cipherAttributes.setKeyAttributes(dataMarkings);
+
+    // initialize chunk cipher object
+    ISChunkCryptoCipherAuto cipher(agent);
+
+    // encrypt
+    std::string ciphertext;
+    nErrorCode = cipher.encrypt(message, ciphertext, &cipherAttributes);
+    if (nErrorCode != ISCRYPTO_OK) {
+        std::cerr << "Encryption Error: " << ISAgentSDKError::getErrorCodeString(nErrorCode) << std::endl;
+        exit(1);
+    }
+
+    // decrypt
+    std::string plaintext;
+    nErrorCode = cipher.decrypt(ciphertext, plaintext);
+    if (nErrorCode != ISCRYPTO_OK) {
+        std::cerr << "Decryption Error: " << ISAgentSDKError::getErrorCodeString(nErrorCode) << std::endl;
+        std::cerr << "You don't have the correct clearance." << std::endl;
+        std::cerr << std::endl;
+        exit(1);
+    }
+
+    // display data
+    std::cout << "Ciphertext : " << ciphertext << std::endl;
+    std::cout << "Plaintext  : " << plaintext << std::endl;
+    std::cout << std::endl;
+}
+
