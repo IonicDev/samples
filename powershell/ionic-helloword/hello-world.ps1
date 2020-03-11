0e44090cac401bcdd8cd84a5dfabb4635ca850f5
Nicolas Vautier <nvautier@ionicsecurity.com>
2020-03-11 13:39:11 -0700

    DX-1409: Updated bin name + formatting

--- powershell/ionic-helloword/hello-world.ps1
+++ powershell/ionic-helloword/hello-world.ps1
@@ -21,13 +21,19 @@ $MESSAGE="'this is a secret message!'"
 echo "ORIGINAL TEXT      : ${MESSAGE}"
 
 <# Encrypt a string (The key is automatically created) #>
-$ENCRYPTED_MESSAGE=$(ionicsdk --devicetype password --devicefile ${PERSISTOR_PATH} --devicepw ${IONIC_PERSISTOR_PASSWORD} `
-    chunk encrypt -s "${MESSAGE}" --metas "${ClientMetadata}")
+$ENCRYPTED_MESSAGE=$(machina `
+  --devicetype password `
+  --devicefile ${PERSISTOR_PATH} `
+  --devicepw ${IONIC_PERSISTOR_PASSWORD} `
+    chunk encrypt --instr "${MESSAGE}" --metas "${ClientMetadata}")
 
 echo "CIPHER TEXT        : ${ENCRYPTED_MESSAGE}"
 
 <# Decrypt a string (The correlating key is automatically fetched) #>
-$MESSAGE=$(ionicsdk --devicetype password --devicefile ${PERSISTOR_PATH} --devicepw ${IONIC_PERSISTOR_PASSWORD} `
-    chunk decrypt -s "${ENCRYPTED_MESSAGE}" --metas "${ClientMetadata}")
+$MESSAGE=$(machina `
+  --devicetype password `
+  --devicefile ${PERSISTOR_PATH} `
+  --devicepw ${IONIC_PERSISTOR_PASSWORD} `
+    chunk decrypt --instr "${ENCRYPTED_MESSAGE}" --metas "${ClientMetadata}")
 
 echo "PLAIN TEXT         : ${MESSAGE}"
