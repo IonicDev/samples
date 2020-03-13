5bad68c9a91de6b750ef28f909111fe565534382
Nicolas Vautier <nvautier@ionicsecurity.com>
2020-03-13 09:26:45 -0700

    DX-1512: Renamed script to match codeSnippet component

--- bash/ionic-helloworld/ionic-helloworld.sh
+++ bash/ionic-helloworld/ionic-helloworld.sh
@@ -3,15 +3,15 @@
 
 # Confirm that the profile persistor password is set as an environment variable
 if [[ -z "${IONIC_PERSISTOR_PASSWORD}" ]]; then
-  echo "[!] Please provide the persistor password as env variable: IONIC_PERSISTOR_PASSWORD"
-  exit 1
+echo "[!] Please provide the persistor password as env variable: IONIC_PERSISTOR_PASSWORD"
+exit 1
 fi
 
 # Confirm that the profile persistor file exists
 PERSISTOR_PATH="${HOME}/.ionicsecurity/profiles.pw"
 if [[ ! -f "$PERSISTOR_PATH" ]]; then
-    echo "[!] '$PERSISTOR_PATH' does not exist"
-    exit 1
+echo "[!] '$PERSISTOR_PATH' does not exist"
+exit 1
 fi
 
 # Configure this script to exit when any command fails
@@ -24,12 +24,15 @@ ClientMetadata="ionic-application-name:Ciphers CLI Tutorial,ionic-application-ve
 MESSAGE='this is a secret message!'
 echo "ORIGINAL TEXT      : ${MESSAGE}"
 
+# Define data markings
+MutableAttrs="'clearance-level:secret'"
+
 # Encrypt a string (The key is automatically created)
 ENCRYPTED_MESSAGE=$(machina \
   --devicetype password \
   --devicefile ${PERSISTOR_PATH} \
   --devicepw ${IONIC_PERSISTOR_PASSWORD} \
-    chunk encrypt --instr "${MESSAGE}" --metas "${ClientMetadata}")
+    chunk encrypt --instr "${MESSAGE}" --mattrs "${MutableAttrs}" --metas "${ClientMetadata}")
 
 echo "CIPHER TEXT        : ${ENCRYPTED_MESSAGE}"
 
