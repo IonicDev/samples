490df14c1ba2bd20af6c69e82b3ca066317162a1
Rick Ehrhart <rick@ionicsecurity.com>
2020-06-22 18:11:13 -0700

    Removing Java JNI files

--- java-jni/enrollment/README.md
+++
@@ -1,3 +0,0 @@
-The sample which currently existed here has been migrated to it's own repository.
-Please see https://github.com/IonicDev/enrollment-example-java.
-
--- java-jni/file-crypto-auto-decrypt/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-IS_SDK_LIB=${IONIC_REPO_ROOT}/ISAgentSDKJava/Lib/Linux/Release/x86_64
-IS_SDK_JAR=${IONIC_REPO_ROOT}/ISAgentSDKJava/Lib/Linux/Release/universal/AgentSdkJava.jar
-
-MAIN_CLASS=FileCryptoAutoDecrypt
-java -Djava.library.path=${IS_SDK_LIB} -cp ${IS_SDK_JAR}:./src/main/java/com/ionic/samples ${MAIN_CLASS} $1 $2
-
