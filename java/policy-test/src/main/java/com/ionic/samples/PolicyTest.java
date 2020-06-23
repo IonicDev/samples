cd2ade46a73a1449031d9032be9634efc1ff8f82
Rick Ehrhart <rick@ionicsecurity.com>
2020-06-23 15:04:23 -0700

    Some missed copyright changes.

--- java/policy-test/src/main/java/com/ionic/samples/PolicyTest.java
+++ java/policy-test/src/main/java/com/ionic/samples/PolicyTest.java
@@ -1,5 +1,5 @@
 /*
- * (c) 2019 Ionic Security Inc.
+ * (c) 2019-2020 Ionic Security Inc.
  * By using this code, I agree to the Terms & Conditions (https://dev.ionic.com/use.html)
  * and the Privacy Policy (https://www.ionic.com/privacy-notice/).
  * 
